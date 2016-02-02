13f0cf7e6088f9f97aa86882777d27b2f7375a98
harryqin1978 <zergling9999@hotmail.com>
2016-02-02 17:56:50 +0800

    first commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=nnf',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8',
+];
