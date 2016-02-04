7d63690a87670ed2d739ec7a43d98780531d58f2
harryqin1978 <zergling9999@hotmail.com>
2016-02-04 17:52:48 +0800

    code update

---
+++ models/Source.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "source".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $order
+ */
+class Source extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'source';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['order'], 'integer'],
+            [['name'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'order' => Yii::t('app', 'Order'),
+        ];
+    }
+}
